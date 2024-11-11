25a8193498fc6f61939a5dbe782fdbea96963311
CheatsCodes <schumacheroficial@gmail.com>
2024-11-11 12:43:22 -0300

    Update Teste1.js

--- Aula/Teste1.js
+++ Aula/Teste1.js
@@ -1,15 +1,29 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync({sigint: true});
 
-const PrecoSucos =5.2;
-const PrecoSanduiches = 12;
+let PrecoSucos =5.2;
+let PrecoSanduiches = 12;
 
 let quantidadeSuco = Number(prompt("Quantos sucos você pegou? "));
-let quantidadeSanduiches = Number(prompt("Quantos sanduiches vocÊ pegou? "));
+let quantidadeSanduiches = Number(prompt("Quantos sanduiches você pegou? "));
+
+let descontoSuco = 0;
+if (quantidadeSuco>10){
+    descontoSuco = PrecoSucos * 0.2 * quantidadeSuco;
+    PrecoSucos = PrecoSucos * 0.8;
+}
 
 let totalSuco = quantidadeSuco * PrecoSucos;
+
+let descontoSanduiche = 0;
+if (quantidadeSanduiches>10){
+    descontoSanduiche = PrecoSanduiches * 0.2 * quantidadeSanduiches;
+    PrecoSanduiches = PrecoSanduiches * 0.8;
+}
 let totalSanduiches = quantidadeSanduiches * PrecoSanduiches;
+
 let totalConta = totalSuco + totalSanduiches;
+let totalDesconto = descontoSuco + descontoSanduiche;
 
 console.log(`
     Recibo:
@@ -18,5 +32,6 @@ console.log(`
     Total dos sucos: R$ ${totalSuco.toFixed(2)}
     Total dos sanduiches: R$ ${totalSanduiches.toFixed(2)}
     ----------------------
+    Desconto aplicado: R$ ${totalDesconto.toFixed(2)}
     Valor total da conta: R$ ${totalConta.toFixed(2)}
 `);
